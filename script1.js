7c321f4fd973a05434389bc9a0943a42229cf3a8
Riko-04 <echoge11@gmail.com>
2024-03-24 01:25:08 +0300

    commit I

--- script1.js
+++ script1.js
@@ -1,18 +1,19 @@
-function calc(){
-    let math = document.getElementById("math").value;
-    eng = document.getElementById("eng").value;
-    lang = document.getElementById("lang").value;
-    hum = document.getElementById("hum").value;
-    tech = document.getElementById("tech").value;
-}
+let math, eng, lang, hum, tech;
 
-let sum = parseFloat(math) + parseFloat(eng) + parseFloat(lang) + parseFloat(hum) + parseFloat(tech);
-document.getElementById("spot").innerHTML = `Total mark is = ${sum}`;
+function calc() {
+    math = parseFloat(document.getElementById("math").value);
+    eng = parseFloat(document.getElementById("eng").value);
+    lang = parseFloat(document.getElementById("lang").value);
+    hum = parseFloat(document.getElementById("hum").value);
+    tech = parseFloat(document.getElementById("tech").value);
 
-let percentage = (sum / 500) * 100;
-document.getAnimations("spot1").innerHTML = `Percentage is = ${percentage}%`;
+    let sum = math + eng + lang + hum + tech;
+    document.getElementById("spot").innerHTML = `Total mark is = ${sum}`;
 
-if (percentage > 79) {
+    let percentage = (sum / 500) * 100;
+    document.getElementById("spot1").innerHTML = `Percentage is = ${percentage}%`;
+
+    if (percentage > 79) {
         document.getElementById("spot2").innerHTML = `Grade: A`;
     } else if (percentage >= 60 && percentage <= 79) {
         document.getElementById("spot2").innerHTML = `Grade: B`;
@@ -20,8 +21,9 @@ if (percentage > 79) {
         document.getElementById("spot2").innerHTML = `Grade: C`;
     } else if (percentage >= 40 && percentage <= 49) {
         document.getElementById("spot2").innerHTML = `Grade: D`;
-    } else if (percentage >=0 && percentage <= 39){
+    } else if (percentage >= 0 && percentage <= 39) {
         document.getElementById("spot2").innerHTML = `Grade: E`;
-    } else{
+    } else {
         document.getElementById("spot2").innerHTML = `ERROR`;
     }
+}
